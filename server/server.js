2eebacd1dce6164f62ffd6f2a7092372dfb1f1e7
aftabalam999 <course.aftabalam@gmail.com>
2025-10-30 14:43:05 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -39,8 +39,10 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: ['https://friendo-nine.vercel.app', 'http://localhost:3000'],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json()); // Parse JSON bodies
